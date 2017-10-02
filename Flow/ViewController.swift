7388206b9a3344d8249b8150a4cde39315ce7db7
Vince <avggonzalescsa@yahoo.com>
2017-10-03 00:38:11 +0800

    init project

---
+++ Flow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flow
+//
+//  Created by Vince on 03/10/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
