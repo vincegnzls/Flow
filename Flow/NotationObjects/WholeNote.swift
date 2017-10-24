d33ad07278a5e95e93bfe3710501a61bc26b7297
Vince <avggonzalescsa@yahoo.com>
2017-10-24 18:47:16 +0800

    updated

---
+++ Flow/NotationObjects/WholeNote.swift
@@ -0,0 +1,21 @@
+//
+//  WholeNote.swift
+//  Flow
+//
+//  Created by Vince on 24/10/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import UIKit
+
+class WholeNote: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
