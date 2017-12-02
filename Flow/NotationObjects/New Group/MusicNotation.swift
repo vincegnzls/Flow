82feb81a93f2f857da437ca0cc9e4ec6504638de
Kevin <kevin98gray@gmail.com>
2017-12-03 02:28:52 +0800

    Merge branch 'music-notation-2'

---
+++ Flow/NotationObjects/New Group/MusicNotation.swift
@@ -0,0 +1,29 @@
+//
+//  MusicNotation.swift
+//  Flow
+//
+//  Created by Kevin Chan on 02/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import UIKit
+
+class MusicNotation {
+    // MARK: Properties
+    var screenCoordinates: ScreenCoordinates?
+    var gridCoordinates: GridCoordinates?
+    var image: UIImage?
+    
+    init(screenCoordinates: ScreenCoordinates?,
+         gridCoordinates: GridCoordinates?) {
+        self.screenCoordinates = screenCoordinates
+        self.gridCoordinates = gridCoordinates
+        
+        self.setImage()
+    }
+    
+    // Sets the image based on the music notation
+    func setImage() {
+        // Do nothing
+    }
+}
