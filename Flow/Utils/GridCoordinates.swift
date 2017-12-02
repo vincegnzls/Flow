aea0c6e10b1eef1a4cedaa51d0aaf9b5b2c03af7
Kevin <kevin98gray@gmail.com>
2017-12-03 03:05:29 +0800

    Fix sana

---
+++ Flow/Utils/GridCoordinates.swift
@@ -0,0 +1,17 @@
+//
+//  GridCoordinates.swift
+//  Flow
+//
+//  Created by Kevin Chan on 03/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+struct GridCoordinates {
+    var col: Int
+    var row: Int
+    
+    init(col: Int, row: Int) {
+        self.col = col
+        self.row = row
+    }
+}
