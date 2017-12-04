9096882faf2a9f2661b954c87a7896b30601e7b8
Kevin <kevin98gray@gmail.com>
2017-12-04 22:49:37 +0800

    Setup necessary classes and methods for parsing and writing

--- Flow/Utils/FileHandling/ParserWriter.swift
+++ Flow/Utils/FileHandling/ParserWriter.swift
@@ -8,10 +8,52 @@
 
 import Foundation
 
+// Singleton class for handling files
 class ParserWriter {
     
-    func readFile(filename: String) -> Composition {
+    static let instance = ParserWriter()
+    
+    // File storage
+    private let documentsDirectory = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask,
+                                                           appropriateFor: nil, create: true)
+    
+    private init() {}
+    
+    func readFile(_ fileName: String) -> Composition {
+        let fileURL = documentsDirectory.appendingPathComponent(fileName).appendingPathExtension("xml")
+        var readString = ""
         
+        do {
+            readString = try String(contentsOf: fileURL)
+        } catch let error as NSError {
+            print("Failed to read file \(fileName)")
+            print(error)
+        }
+        
+        return convertMusicXMLtoComposition(readString)
+    }
+    
+    func writeToFile(composition: Composition, fileName: String) {
+        let fileURL = documentsDirectory.appendingPathComponent(fileName).appendingPathExtension("xml")
+        
+        let writeString = convertCompositionToMusicXML(composition)
+        do {
+            try writeString.write(to: fileURL, atomically: true, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("Failed to write to file \(fileName)")
+            print(error)
+        }
+    }
+    
+    func convertCompositionToMusicXML(_ composition: Composition) -> String {
+        let xml = AEXMLDocument()
+        // Perform conversion here
+        
+        return xml.xml
+    }
+    
+    func convertMusicXMLtoComposition(_ xml: String) -> Composition {
+        // Perform conversion here
         
         return Composition()
     }
