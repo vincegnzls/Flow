55368a5b78d2b39ab76d0e99b64dc9539db63437
Kevin <kevin98gray@gmail.com>
2017-12-10 22:21:05 +0800

    Create separate view controller for start menu

---
+++ Flow/StartMenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartMenuViewController.swift
+//  Flow
+//
+//  Created by Kevin Chan on 10/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import UIKit
+
+class StartMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
