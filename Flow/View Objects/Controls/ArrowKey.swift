1e4ba20f5387ee8f477dc33c671f54f9952d5717
Kevin <kevin98gray@gmail.com>
2017-12-10 13:55:11 +0800

    Set up event posting for arrow key pressed event

---
+++ Flow/View Objects/Controls/ArrowKey.swift
@@ -0,0 +1,10 @@
+//
+//  ArrowKey.swift
+//  Flow
+//
+//  Created by Kevin Chan on 10/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+enum ArrowKey {
+    case up, down, left, right
+}
