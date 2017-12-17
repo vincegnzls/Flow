f4ed16135893e5ba3294a752a6e1e7021e921f61
Vince <avggonzalescsa@yahoo.com>
2017-12-17 18:42:12 +0800

    added Time Signature popup

---
+++ Flow/TimeSignatureViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TimeSignatureViewController.swift
+//  Flow
+//
+//  Created by Vince on 17/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import UIKit
+
+class TimeSignatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onDismissPopup(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func onSavePress(_ sender: Any) {
+        dismiss(animated: true) {
+            //asd
+        }
+    }
+    
+    @IBAction func onCancelPress(_ sender: Any) {
+        dismiss(animated: true) {
+            //asd
+        }
+    }
+}
