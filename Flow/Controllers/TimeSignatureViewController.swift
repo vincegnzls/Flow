19201a1e0cc828f52bf967d345a80e07b59bf02a
Vince <avggonzalescsa@yahoo.com>
2017-12-18 22:59:59 +0800

    cleaned code

--- Flow/Controllers/TimeSignatureViewController.swift
+++ Flow/Controllers/TimeSignatureViewController.swift
@@ -15,42 +15,53 @@ class TimeSignatureViewController: UIViewController {
     @IBOutlet weak var keySignaturePicker: UIPickerView!
     
     let keySignatureHandler = KeySignaturePicker()
-    
     var rotationAngle: CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupKeySignaturePicker()
+    }
+    
+    func setupKeySignaturePicker() {
+        // Get data
         keySignatureHandler.keySignatures = KeySignatureData.getData()
         
+        // Rotate picker for horizontal view
         rotationAngle = -90 * (.pi / 180)
-        
         keySignaturePicker.transform = CGAffineTransform(rotationAngle: rotationAngle)
-
+        
+        // Assign delegate and data source
         keySignaturePicker.delegate = keySignatureHandler
         keySignaturePicker.dataSource = keySignatureHandler
     }
 
+    // When a user taps outside the popup
     @IBAction func onDismissPopup(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    // When a user taps save
     @IBAction func onSavePress(_ sender: Any) {
         dismiss(animated: true) {
             //asd
         }
     }
     
+    // When a user taps cancel
     @IBAction func onCancelPress(_ sender: Any) {
         dismiss(animated: true) {
             //asd
         }
     }
     
+    // Updates label whenever number of beats is changed
     @IBAction func onChangeNBeats(_ sender: UISlider) {
         nBeatsLabel.text = String(Int(sender.value))
     }
     
+    
+    // Updates label whenever number of beats is changed
     @IBAction func onChangeBeatDuration(_ sender: UISlider) {
         beatDurationLabel.text = String(Int(sender.value) * 2)
     }
--- Flow/View Objects/KeySignaturePicker.swift
+++ Flow/View Objects/KeySignaturePicker.swift
@@ -36,28 +36,29 @@ extension KeySignaturePicker: UIPickerViewDelegate {
         
         let view = UIView(frame: CGRect(x: 0, y: 0, width: customWidth, height: customHeight ))
         
+        // Adds key letter as a label
         let keySignatureLabel = UILabel(frame: CGRect(x: 0, y: 0, width: customWidth, height: 30))
         
         keySignatureLabel.text = keySignatures[row].key.rawValue
         keySignatureLabel.textColor = .white
         keySignatureLabel.textAlignment = .center
         keySignatureLabel.font = UIFont.systemFont(ofSize: 25, weight: UIFont.Weight.light)
-        
         view.addSubview(keySignatureLabel)
         
+        // Adds the key signature image
         let keySignatureImage = UIImage(named: "sharp-white") // change to keySignatures[row].image
-        
         let keySignatureImageView = UIImageView(frame: CGRect(x: (customWidth / 2) - ((keySignatureImage?.size.width)! / 2), y: 40, width: (keySignatureImage?.size.width)!, height: (keySignatureImage?.size.height)!))
         keySignatureImageView.image = keySignatureImage
-        
         view.addSubview(keySignatureImageView)
         
+        // Rotate each key signature view for horizontal orientation
         view.transform = CGAffineTransform(rotationAngle: (90 * (.pi / 180)))
         
         return view
         
     }
     
+    // Listener when key signature is changed
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         print(keySignatures[row].key.rawValue)
     }
