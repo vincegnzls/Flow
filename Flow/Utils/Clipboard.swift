8bf05ea6408a4a71109cb6b6543cc86274718e82
Kevin <kevin98gray@gmail.com>
2017-12-21 00:16:17 +0800

    Add Clipboard class for cut/copy/paste

---
+++ Flow/Utils/Clipboard.swift
@@ -0,0 +1,22 @@
+//
+//  Clipboard.swift
+//  Flow
+//
+//  Created by Kevin Chan on 21/12/2017.
+//  Copyright © 2017 MusicG. All rights reserved.
+//
+
+import Foundation
+
+class Clipboard {
+    
+    // MARK: Shared instance
+    static let instance = Clipboard()
+    
+    // MARK: Properties
+    private var items: [MusicNotation]
+    
+    private init() {
+        items = []
+    }
+}
