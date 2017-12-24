c754e2fcbbe806fc7e90a4b4007bcfd5e32ea4f4
Kevin <kevin98gray@gmail.com>
2017-12-24 17:23:27 +0800

    Implement functions for undo and redo manager

--- Flow/Utils/UndoRedo.swift
+++ Flow/Utils/UndoRedo.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 class UndoRedo {
+    // MARK: Constants
+    private static let MAX_ACTIONS = 10
     
     // MARK: Shared instance
     static let instance = UndoRedo()
@@ -23,10 +25,34 @@ class UndoRedo {
     }
     
     func undo() {
-        
+        if let action = self.undoStack.popLast() {
+            action.undo()
+            self.addActionToRedoStack(action)
+        }
     }
     
     func redo() {
+        if let action = self.redoStack.popLast() {
+            action.redo()
+            self.addActionToUndoStack(action)
+        }
+    }
+    
+    func addActionToUndoStack(_ action: Action) {
+        self.undoStack.append(action)
         
+        // Limit number of actions stored to save memory
+        if self.undoStack.count > UndoRedo.MAX_ACTIONS {
+            self.undoStack.removeFirst()
+        }
+    }
+    
+    private func addActionToRedoStack(_ action: Action) {
+        self.redoStack.append(action)
+        
+        // Limit number of actions stored to save memory
+        if self.redoStack.count > UndoRedo.MAX_ACTIONS {
+            self.redoStack.removeFirst()
+        }
     }
 }
