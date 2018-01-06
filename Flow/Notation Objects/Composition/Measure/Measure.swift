4540b6875d8bc072b468c3d5736f70ddca05eeab
Vince <avggonzalescsa@yahoo.com>
2018-01-06 16:19:32 +0800

    implemented disabling of adding notes

--- Flow/Notation Objects/Composition/Measure/Measure.swift
+++ Flow/Notation Objects/Composition/Measure/Measure.swift
@@ -15,6 +15,7 @@ class Measure {
     var notationObjects: [MusicNotation]
     var bounds: Bounds
     var curBeatValue: Float
+    var validNotes: [MusicNotation]
     
     init(keySignature: KeySignature = .c,
          timeSignature: TimeSignature = TimeSignature(),
@@ -26,14 +27,22 @@ class Measure {
         self.notationObjects = notationObjects
         self.bounds = Bounds()
         self.curBeatValue = 0
+        self.validNotes = [MusicNotation]()
+        
+        initValidNotes()
     }
     
     public func addNoteInMeasure (_ musicNotation: MusicNotation) {
         
+        print("ADD NOTE")
+        
         if(isAddNoteValid(musicNotation: musicNotation)) {
-            
+            print("ADD NOTE VALID")
+        
             self.curBeatValue += musicNotation.type.getBeatValue()
             notationObjects.append(musicNotation)
+            
+            updateValidNotes(validNotes: getValidNotes()) // update valid notes in notation controls after adding note
             
         } else {
             
@@ -43,9 +52,72 @@ class Measure {
         
     }
     
+    public func initValidNotes() {
+        self.validNotes.append(MusicNotation(type: RestNoteType.whole))
+        self.validNotes.append(MusicNotation(type: RestNoteType.half))
+        self.validNotes.append(MusicNotation(type: RestNoteType.quarter))
+        self.validNotes.append(MusicNotation(type: RestNoteType.eighth))
+        self.validNotes.append(MusicNotation(type: RestNoteType.sixteenth))
+        self.validNotes.append(MusicNotation(type: RestNoteType.thirtySecond))
+        self.validNotes.append(MusicNotation(type: RestNoteType.sixtyFourth))
+    }
+    
+    public func getValidNotes() -> [MusicNotation] {
+        
+        if RestNoteType.whole.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[0].valid = false
+        } else {
+            validNotes[0].valid = true
+        }
+        if RestNoteType.half.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+             validNotes[1].valid = false
+        } else {
+            validNotes[1].valid = true
+        }
+        if RestNoteType.quarter.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[2].valid = false
+        } else {
+            validNotes[2].valid = true
+        }
+        if RestNoteType.eighth.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[3].valid = false
+        } else {
+            validNotes[3].valid = false
+        }
+        if RestNoteType.sixteenth.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[4].valid = false
+        } else {
+            validNotes[4].valid = true
+        }
+        if RestNoteType.thirtySecond.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[5].valid = false
+        } else {
+            validNotes[5].valid = true
+        }
+        if RestNoteType.sixtyFourth.getBeatValue() + curBeatValue > timeSignature.getMaxBeatValue() {
+            validNotes[6].valid = false
+        } else {
+            validNotes[6].valid = true
+        }
+        
+        return validNotes
+    }
+    
+    // send event to notation controls
+    public func updateValidNotes(validNotes: [MusicNotation]) {
+        
+        let params = Parameters()
+        
+        params.put(key: KeyNames.VALID_NOTES, value: validNotes)
+        EventBroadcaster.instance.postEvent(event: EventNames.UPDATE_VALID_NOTES, params: params)
+        
+    }
+    
     public func isAddNoteValid (musicNotation: MusicNotation) -> Bool {
         
-        return self.curBeatValue <= self.timeSignature.getMaxBeatValue()
+        return self.curBeatValue + musicNotation.type.getBeatValue() <= self.timeSignature.getMaxBeatValue()
             
     }
+    
+    
 }
--- Flow/View Objects/Controls/NotationControlsView.swift
+++ Flow/View Objects/Controls/NotationControlsView.swift
@@ -10,7 +10,22 @@ import UIKit
 
 @IBDesignable
 class NotationControlsView: UIView {
-
+    
+    @IBOutlet var wholeNote: UIButton!
+    @IBOutlet var wholeRest: UIButton!
+    @IBOutlet var halfNote: UIButton!
+    @IBOutlet var halfRest: UIButton!
+    @IBOutlet var quarterNote: UIButton!
+    @IBOutlet var quarterRest: UIButton!
+    @IBOutlet var eighthNote: UIButton!
+    @IBOutlet var eighthRest: UIButton!
+    @IBOutlet var sixteenthNote: UIButton!
+    @IBOutlet var sixteenthRest: UIButton!
+    @IBOutlet var thirtySecondNote: UIButton!
+    @IBOutlet var thirtySecondRest: UIButton!
+    @IBOutlet var sixtyFourthNote: UIButton!
+    @IBOutlet var sixtyFourthRest: UIButton!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.setup()
@@ -32,6 +47,9 @@ class NotationControlsView: UIView {
         self.layer.shadowOpacity = 0.1
         self.layer.shadowOffset = CGSize.zero
         self.layer.shadowRadius = 5
+        
+        EventBroadcaster.instance.addObserver(event: EventNames.UPDATE_VALID_NOTES,
+                                              observer: Observer(id: "NotationControls.updateValidNotes", function: self.updateValidNotes))
     }
     
     @IBAction func wholeNote(_ sender: ButtonEffect) {
@@ -120,4 +138,61 @@ class NotationControlsView: UIView {
         params.put(key: KeyNames.IS_REST_KEY, value: isRest)
         EventBroadcaster.instance.postEvent(event: EventNames.NOTATION_KEY_PRESSED, params: params)
     }
+    
+    func updateValidNotes(params: Parameters) {
+        let validNotes:[MusicNotation] = params.get(key: KeyNames.VALID_NOTES) as! [MusicNotation]
+        
+        for note in validNotes {
+            
+            if note.valid {
+                if note.type == RestNoteType.whole {
+                    wholeNote.isEnabled = true
+                    wholeRest.isEnabled = true
+                } else if note.type == RestNoteType.half {
+                    halfNote.isEnabled = true
+                    halfRest.isEnabled = true
+                } else if note.type == RestNoteType.quarter {
+                    quarterNote.isEnabled = true
+                    quarterRest.isEnabled = true
+                } else if note.type == RestNoteType.eighth {
+                    eighthNote.isEnabled = true
+                    eighthRest.isEnabled = true
+                } else if note.type == RestNoteType.sixteenth {
+                    sixteenthNote.isEnabled = true
+                    sixteenthRest.isEnabled = true
+                } else if note.type == RestNoteType.thirtySecond {
+                    thirtySecondNote.isEnabled = true
+                    thirtySecondRest.isEnabled = true
+                } else if note.type == RestNoteType.sixtyFourth {
+                    sixtyFourthNote.isEnabled = true
+                    sixtyFourthRest.isEnabled = true
+                }
+            } else {
+                if note.type == RestNoteType.whole {
+                    wholeNote.isEnabled = false
+                    wholeRest.isEnabled = false
+                } else if note.type == RestNoteType.half {
+                    halfNote.isEnabled = false
+                    halfRest.isEnabled = false
+                } else if note.type == RestNoteType.quarter {
+                    quarterNote.isEnabled = false
+                    quarterRest.isEnabled = false
+                } else if note.type == RestNoteType.eighth {
+                    eighthNote.isEnabled = false
+                    eighthRest.isEnabled = false
+                } else if note.type == RestNoteType.sixteenth {
+                    sixteenthNote.isEnabled = false
+                    sixteenthRest.isEnabled = false
+                } else if note.type == RestNoteType.thirtySecond {
+                    thirtySecondNote.isEnabled = false
+                    thirtySecondRest.isEnabled = false
+                } else if note.type == RestNoteType.sixtyFourth {
+                    sixtyFourthNote.isEnabled = false
+                    sixtyFourthRest.isEnabled = false
+                }
+            }
+            
+        }
+    }
+    
 }
