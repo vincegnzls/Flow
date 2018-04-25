9615b282ca392aa8170ccf290bb74c6f4a68df97
Kevin <kevin98gray@gmail.com>
2018-04-25 18:46:46 +0800

    Setup EditSignatureAction

---
+++ Flow/Notation Objects/Action/EditSignatureAction.swift
@@ -0,0 +1,54 @@
+//
+//  EditSignatureAction.swift
+//  Flow
+//
+//  Created by Kevin Chan on 25/04/2018.
+//  Copyright © 2018 MusicG. All rights reserved.
+//
+
+import Foundation
+
+class EditSignatureAction: Action {
+    
+    var measures: [Measure]
+    var oldKeySignature: KeySignature
+    var oldTimeSignature: TimeSignature
+    var newKeySignature: KeySignature
+    var newTimeSignature: TimeSignature
+    
+    init(measures: [Measure],
+         oldKeySignature: KeySignature,
+         oldTimeSignature: TimeSignature,
+         newKeySignature: KeySignature? = nil,
+         newTimeSignature: TimeSignature? = nil) {
+        self.measures = measures
+        self.oldKeySignature = oldKeySignature
+        self.oldTimeSignature = oldTimeSignature
+        if let new = newKeySignature {
+            self.newKeySignature = new
+        } else {
+            self.newKeySignature = oldKeySignature
+        }
+        
+        if let new = newTimeSignature {
+            self.newTimeSignature = new
+        } else {
+            self.newTimeSignature = oldTimeSignature
+        }
+        
+    }
+    
+    func execute() {
+        // Code here
+    }
+    
+    func undo() {
+        // Code here
+    }
+    
+    func redo() {
+        // Code here
+    }
+    
+    
+}
