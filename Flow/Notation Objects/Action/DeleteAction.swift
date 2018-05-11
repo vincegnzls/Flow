73cfca28dd1cb1bae85d6011992043fd48f43050
Patrick Tobias <patrick_tobias@outlook.com>
2018-05-11 14:48:48 +0800

    Implement deletion of single notes in chords

--- Flow/Notation Objects/Action/DeleteAction.swift
+++ Flow/Notation Objects/Action/DeleteAction.swift
@@ -22,10 +22,18 @@ class DeleteAction: Action {
 
     func execute() {
         for notation in self.notations {
-            if let measure = notation.measure {
+            if let measure = notation.measure { // NOTES in CHORDS aren't assigned to a measure, so it surpasses this check
                 self.indices.append(measure.notationObjects.index(of: notation)!)
                 measure.remove(notation)
                 self.measures.append(measure)
+            } else if let note = notation as? Note { // FOR DELETING GROUP OF NOTES FROM CHORDS
+                if let chord = note.chord {
+                    if let measure = chord.measure {
+                        self.indices.append(measure.notationObjects.index(of: chord)!)
+                        chord.removeNote(note: note)
+                        self.measures.append(measure)
+                    }
+                }
             }
         }
         UndoRedoManager.instance.addActionToUndoStack(self)
@@ -37,15 +45,49 @@ class DeleteAction: Action {
         print("Number of indices: \(self.indices.count)")
         for (i, measure) in self.measures.enumerated() {
             let notation = self.notations[i]
-            let index = self.indices[i]
-            measure.add(notation, at: index)
+            
+            if let note = notation as? Note {
+                if let previousChord = note.chord {
+                    let index = self.indices[i]
+                    
+                    if measure.notationObjects[index] is Chord {
+                        
+                        let chord = measure.notationObjects[index] as? Chord
+                        
+                        chord?.notes.append(note)
+                        
+                    } else if measure.notationObjects[index] is Note {
+                        
+                        if let existingNote = measure.notationObjects[index] as? Note {
+                            previousChord.notes.append(note)
+                            existingNote.chord = previousChord
+                            
+                            measure.replace(existingNote, previousChord)
+                        }
+                        
+                    }
+                    
+                } else {
+                    let index = self.indices[i]
+                    measure.add(notation, at: index)
+                }
+            } else {
+                let index = self.indices[i]
+                measure.add(notation, at: index)
+            }
         }
     }
     
     func redo() {
         for notation in self.notations {
-            if let measure = notation.measure {
+            if let measure = notation.measure { // NOTES in CHORDS aren't assigned to a measure, so it surpasses this check
                 measure.remove(notation)
+            } else if let note = notation as? Note { // FOR DELETING GROUP OF NOTES FROM CHORDS
+                if let chord = note.chord {
+                    if let measure = chord.measure {
+                        chord.removeNote(note: note)
+                    }
+                }
             }
         }
     }
